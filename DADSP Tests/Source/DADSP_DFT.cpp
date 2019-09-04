7ed900e4e987dd8087abdbdf49bcc95ef80b7be0
Johnny Lopez <jalopez@berklee.edu>
2019-09-03 22:47:05 -0600

    Developed the main process quasi sudo

--- DADSP Tests/Source/DADSP_DFT.cpp
+++ DADSP Tests/Source/DADSP_DFT.cpp
@@ -9,3 +9,32 @@
 */
 
 #include "DADSP_DFT.h"
+
+DADSP_DFT::DADSP_DFT()
+{
+    
+}
+
+DADSP_DFT::DADSP_DFT( int N ) :
+N(N)
+{
+    
+}
+
+DADSP_DFT::~DADSP_DFT()
+{
+    
+}
+
+
+void DADSP_DFT::process(float *inBuffer, float *outBuffer, double *REX, double *IMX, int windowSize)
+{
+    for(int i = 0; i < N; i++)
+    {
+        for(int k = 0; k < (N/2 + 1); k++)
+        {
+            outBuffer[i] = REX[k] * cos((2*M_PI*i*k)/N);
+            outBuffer[i] = IMX[k] * sin((2*M_PI*i*k)/N);
+        }
+    }
+}
--- DADSP Tests/Source/DADSP_DFT.h
+++ DADSP Tests/Source/DADSP_DFT.h
@@ -9,3 +9,29 @@
 */
 
 #pragma once
+#include "math.h"
+
+#define DEFAULT_WINDOW_SIZE 512
+#define DEFAULT_HALF_WINDOW (DEFAULT_WINDOW_SIZE/2 + 1)
+
+#ifndef M_PI
+#define M_PI 3.14159265359
+#endif
+
+
+class DADSP_DFT
+{
+public:
+    
+    DADSP_DFT();
+    DADSP_DFT(int N);
+    ~DADSP_DFT();
+    
+    void process(float* inBuffer, float* outBuffer, double* REX, double* IMX, int windowSize);
+    
+    
+private:
+    double mSynthesized[DEFAULT_WINDOW_SIZE] = {0};
+    int N = DEFAULT_WINDOW_SIZE;
+    
+};
